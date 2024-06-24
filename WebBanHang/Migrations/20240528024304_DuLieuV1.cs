7a7689f04d70a19f8e4009d3ec64b0d40fc41cfa
ADMIN <ADMIN@PC213>
2024-06-24 10:28:15 +0700

    Add project files.

---
+++ WebBanHang/Migrations/20240528024304_DuLieuV1.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebBanHang.Migrations
+{
+    public partial class DuLieuV1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[] { 1, 1, "Iphone" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[] { 2, 2, "SamSung" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[] { 3, 3, "XaoMi" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, 1, null, null, "Iphone 7", 300.0 },
+                    { 2, 1, null, null, "Iphone 7s", 350.0 },
+                    { 3, 1, null, null, "Iphone 8", 400.0 },
+                    { 4, 1, null, null, "Iphone 8s", 420.0 },
+                    { 5, 1, null, null, "Iphone 11", 630.0 },
+                    { 6, 1, null, null, "Iphone 11 Pro", 750.0 },
+                    { 7, 1, null, null, "Iphone 14", 820.0 },
+                    { 8, 1, null, null, "Iphone 14 Pro", 950.0 },
+                    { 9, 1, null, null, "Iphone 15", 1200.0 },
+                    { 10, 1, null, null, "Iphone 15 Pro Max ", 1450.0 },
+                    { 11, 2, null, null, "SamSung Gen 10", 750.0 },
+                    { 12, 2, null, null, "SamSung 11", 1250.0 },
+                     { 13, 2, null, null, "SamSung A50", 1250.0 },
+                      { 14, 2, null, null, "SamSung A11s", 1250.0 },
+                       { 15, 3, null, null, "XaMi", 1250.0 },
+                        { 16, 3, null, null, "XaoMi 11s", 1250.0 },
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
---
+++ WebBanHang/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebBanHang.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        [Required (ErrorMessage ="vui lòng nhập đủ")]
+        public string CustomerName { get; set; }
+        [Required(ErrorMessage = "vui lòng nhập đủ")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "vui lòng nhập đủ")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "vui lòng nhập đủ")]
+        public double Total { set; get; }
+        public string State { set; get; }
+
+
+
+    }
+}
